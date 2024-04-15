474fdb530c49e3bf10f73e1c49fd35a68695e049
moseskippe <mkippe@credable.io>
2024-04-15 11:15:05 +0300

    implemented sorting

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,4 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0; //inclusive
+    let hi = haystack.length; //exclusive
+
+    do {
+        const m = Math.floor(lo + (hi - lo)/2);
+        const v = haystack[m];
+        
+        if(v === needle) {
+            return true;
+        } else if (v >  needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    } while (lo < hi);
     
-    return true;
+    return false;
 }
